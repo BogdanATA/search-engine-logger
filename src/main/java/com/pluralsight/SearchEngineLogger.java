c00ccfa6cdef7bbdc9df20a6c48b6c06b43a9399
BogdanATA <batameev@gmail.com>
2026-04-21 15:54:17 -0400

    - created while loop to keep program running unless user exits - added ability for user to press x to exit program - when user searches something logAction first prints search : followed by whatever the user wrote

--- src/main/java/com/pluralsight/SearchEngineLogger.java
+++ src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -11,9 +11,24 @@ public class SearchEngineLogger {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        logAction("Application Launch");
-    }
+        //prints Launch each time it is launched
+        logAction("Launch");
 
+        boolean isDone = false;
+        while (!isDone) {
+            System.out.println("Enter a search term (X to exit)");
+            String searchTerm = scanner.nextLine();
+
+            if (searchTerm.trim().equalsIgnoreCase("X")) { // press x to exit
+                logAction("Exit"); // prints exit if user pressed x
+                isDone = true; // closes loop if user pressed x
+            } else {
+                logAction("search : " + searchTerm); // prints search : followed by the user input
+            }
+        }
+        scanner.close();
+    }
+    // creates buffered writer and writes date and time + all input onto the logs.txt
     public static void logAction(String text) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt", true));
@@ -21,7 +36,7 @@ public class SearchEngineLogger {
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-            writer.write(now.format(dateTimeFormatter) + " " + text + "\n");
+            writer.write(now.format(dateTimeFormatter) + " " + text + "\n"); // prints date and time first followed by user input
 
             writer.close();
         } catch (IOException e) {
