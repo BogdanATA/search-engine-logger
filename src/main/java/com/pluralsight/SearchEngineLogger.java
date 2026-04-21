ffc4b76faad4785fcd92893c5726ea1442e26e67
BogdanATA <batameev@gmail.com>
2026-04-21 15:35:05 -0400

    - created logAction method to take in text and write it into logs.txt including date and time. - called logAction at start of program to print Application Launch each time its runs

--- src/main/java/com/pluralsight/SearchEngineLogger.java
+++ src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class SearchEngineLogger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        logAction("Application Launch");
+    }
+
+    public static void logAction(String text) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt", true));
+
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            writer.write(now.format(dateTimeFormatter) + " " + text + "\n");
+
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error");
+        }
+    }
 }
